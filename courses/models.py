4659b1ef389f9a41f639858874f2e6a72ca2e227
violetta <lotsmanovavioletta@gmail.com>
2023-09-13 20:56:36 +0300

    add models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=250, verbose_name='название')
+    prewie = models.ImageField(upload_to='courses/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=250, verbose_name='название')
+    prewie = models.ImageField(upload_to='courses/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание')
+    link_video = models.CharField(max_length=250, verbose_name='ссылка')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
