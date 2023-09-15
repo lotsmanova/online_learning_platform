1529c56997c1028453612c73cd176562c06d02ef
violetta <lotsmanovavioletta@gmail.com>
2023-09-15 20:30:35 +0300

    add history payments for user

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from course.serializers import PaymentsSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentsSerializer(source='payments_set', many=True)
+
     class Meta:
         model = User
         fields = '__all__'
