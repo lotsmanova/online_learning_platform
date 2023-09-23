61c4d929c281b1de4d9793983c1c547011a66a32
violetta <lotsmanovavioletta@gmail.com>
2023-09-23 10:56:45 +0300

    add model Subscribe

--- course/models.py
+++ course/models.py
@@ -57,3 +57,18 @@ class Payments(models.Model):
         verbose_name = 'платеж'
         verbose_name_plural = 'платежи'
 
+
+class Subscribe_update(models.Model):
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='ссылка на курс')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
+    is_update = models.BooleanField(verbose_name='подписка на обновления', **NULLABLE)
+
+
+    def __str__(self):
+        return f'{self.course} - {self.is_update}'
+
+
+    class Meta:
+        verbose_name = 'подписка на обновления'
+        verbose_name_plural = 'подписки на обновления'
