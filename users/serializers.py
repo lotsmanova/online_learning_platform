c6da26d979470dd9f3e62253939dae4bf2d4089c
violetta <lotsmanovavioletta@gmail.com>
2023-09-24 14:21:48 +0300

    add test user

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    payments = PaymentsSerializer(source='payments_set', many=True)
+    payments = PaymentsSerializer(source='payments_set', many=True, read_only=True)
 
     class Meta:
         model = User
