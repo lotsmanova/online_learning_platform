4f5c1d02ec3aa2623f8e7fe505a2fb77dbe7a417
violetta <lotsmanovavioletta@gmail.com>
2023-09-30 22:01:19 +0300

    check active user

--- subscribes/tasks.py
+++ subscribes/tasks.py
@@ -1,9 +1,11 @@
+from datetime import timedelta
+
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
-
+from django.utils import timezone
 from course.models import Course, Lesson
-from subscribes.models import SubscribeUpdate
+from users.models import User
 
 
 @shared_task
@@ -20,3 +22,11 @@ def sending_mail(pk, model):
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[instance.owner.email]
         )
+
+
+def check_active(*args, **kwargs):
+    users = User.objects.filter(is_active=True)
+    for user in users:
+        if user.last_login - timezone.now() > timedelta(days=30):
+            user.is_active = False
+            user.save()
